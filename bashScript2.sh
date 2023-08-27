4f70b89a3b70408caa42f811778ccb8499b5fee0
0303vikas <2751vikassing@gmail.com>
2023-08-27 16:42:19 +0300

    adding solution to exercise 2

---
+++ bashScript2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+if [ "$#" == 0 ]; then
+    echo "$#"
+    echo "Error: Atleast one argument is required. Which is the path to the git repository."
+    exit 1
+elif [ ! -d $1 ]; then
+    echo "Error: The path provided is not a directory."
+    exit 1
+# elif [ ! -d $1/.git ]; then
+#     echo "Error: The directory provided is not a git repository."
+#     exit 1
+elif [ $# == 1 ]; then
+    echo "Error: Atlease on argument that states the name of the react component is required."
+    exit 1
+else
+    cd $1
+    for arg in "${@:2}"; do
+        count=$(grep -r -c "<$arg" | awk -F ':' '{sum+=$2} END {print sum}')
+        echo "$arg: $count" >>../result2.txt
+    done
+fi
